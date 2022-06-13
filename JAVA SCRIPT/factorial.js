935098960dfb4be523b8d5b838f19165eae6ed8f
Karthikeya <karthikparnam@gmail.com>
2022-06-13 15:26:43 +0530

    Commited this code First Time

---
+++ JAVA SCRIPT/factorial.js
@@ -0,0 +1,22 @@
+function fact(){  
+
+  var initialnum, inputnum,fact;
+
+  fact = 1;  
+
+  inputnum = document.getElementById("num").value;  
+
+  for(initialnum = 1; initialnum <= inputnum; initialnum++)    
+
+  {  
+
+  fact = fact * initialnum;  
+
+  }
+
+  initialnum = initialnum - 1;  
+
+  document.getElementById("res").innerHTML = "The factorial of the number " + initialnum + " is: " + fact ;
+  document.getElementById("res1").innerHTML = alert("The factorial of the number " + initialnum + " is: " + fact );  
+
+}  
